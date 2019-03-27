6d63dd9fe8b41f43376f38414c6346570758252b
Zishan Ali <zishanali06@gmail.com>
2019-03-27 09:58:20 -0500

    doing advanced

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 let express = require('express');
 let cors = require('cors');
 let app = express();
+let path = require('path');
 let indexRouter = require('./routes');
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api', indexRouter);
+
+app.use(express.static(path.join(__dirname, "../client")));
 
 app.listen(3000);
